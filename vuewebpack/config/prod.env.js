2606110eedd5e2438585a46706de74dd8d93c3d2
cd <x19921111@gmail.com>
2019-11-20 16:12:31 +0800

    修改config-prod.env.js/index.js

--- vuewebpack/config/prod.env.js
+++ vuewebpack/config/prod.env.js
@@ -1,4 +1,16 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  APIPATH: '"https://vue-course-api.hexschool.io"', // 單引號中間要包雙引號
+  CUSTOMPATH:'"mlh_test"', 
 }
+
+
+// const merge = require('webpack-merge')
+// const prodEnv = require('./prod.env')
+
+// module.exports = merge(prodEnv, {
+//   NODE_ENV: '"development"',
+//   APIPATH: '"https://vue-course-api.hexschool.io"', // 單引號中間要包雙引號
+//   CUSTOMPATH:'"mlh_test"', 
+// })
